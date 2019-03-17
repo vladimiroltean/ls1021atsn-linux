499a1903181c9e25b75dbc56054f951b62666c43
Vladimir Oltean <olteanv@gmail.com>
2019-03-18 01:22:34 +0200

    net: dsa: Store vlan_filtering as a property of dsa_port
    This allows drivers to query the VLAN setting imposed by the bridge
    driver directly from DSA, instead of keeping their own state based on
    the .port_vlan_filtering callback.
    
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>

--- include/net/dsa.h
+++ include/net/dsa.h
@@ -142,6 +142,7 @@ struct dsa_port {
 	const struct dsa_port	*cpu_dp;
 	struct device_node	*dn;
 	unsigned int		ageing_time;
+	bool			vlan_filtering;
 	u8			stp_state;
 	struct net_device	*bridge_dev;
 	struct devlink_port	devlink_port;
